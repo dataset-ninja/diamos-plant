7df10e3869e892c4e6ed3e74ccdf9992f3d01776
peixed351 <120389559+peixed351@users.noreply.github.com>
2023-12-29 18:04:09 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://zenodo.org/records/5557313#.YgZSYHVBzmg"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 11886837
+PREVIEW_IMAGE_ID: int = 11890078
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/diamos-plant"
