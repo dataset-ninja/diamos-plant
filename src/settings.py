eb487b7c939c3fbeb6a9f9246265c62295fcdd51
peixed351 <peixed351@gmail.com>
2023-12-29 14:59:56 +0300

    add all stats

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "DiaMOS Plant"
 PROJECT_NAME_FULL: str = "DiaMOS Plant: A Dataset for Diagnosis and Monitoring Plant Disease"
-HIDE_DATASET = False  # set False when 100% sure about repo quality
+HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
